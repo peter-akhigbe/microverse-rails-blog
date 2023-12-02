76b0468833b82fe82dd9386511b280cb950e8366
Peter Akhigbe <akhigbe37@gmail.com>
2023-12-02 16:38:49 +0100

    Create and set up associations between models

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  def recent_posts(limit = 3)
+    posts.order(created_at: :desc).limit(limit)
+    puts 'Method is working as expected'
+  end
+end
