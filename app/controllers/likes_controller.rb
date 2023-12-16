d4903748984667c56f8f25f83afe843e0e3bd8a3
Peter Akhigbe <akhigbe37@gmail.com>
2023-12-16 18:53:51 +0100

    Fix like bug in likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @user = current_user
-    @like = Like.new(author_id: @user.id, post_id: @post.id)
+    @like = Like.new(user_id: @user.id, post_id: @post.id)
     return unless @like.save
 
     redirect_to user_post_path(@user.id, @post)
