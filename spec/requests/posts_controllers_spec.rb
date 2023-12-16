131ed3fc843fa8301e59cf1710b04fed2dfe4ad3
Peter Akhigbe <akhigbe37@gmail.com>
2023-12-16 10:20:03 +0100

    Create tests for posts requests

---
+++ spec/requests/posts_controllers_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts controller', type: :request do
+  describe 'GET /users/user_id/posts' do
+    before do
+      user = User.create(name: 'Ben')
+      get user_posts_path(user)
+    end
+
+    it 'returns a 200 status code' do
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the 'index' template" do
+      expect(response).to render_template('index')
+    end
+
+    it "includes 'All posts by User' in the response body" do
+      expect(response.body).to include('All Posts by Ben')
+    end
+  end
+
+  describe 'GET /users/user_id/posts/post_id' do
+    before do
+      user = User.create(name: 'Tom')
+      post = user.posts.create(title: 'test post', text: 'test post body')
+      get user_post_path(user, post)
+    end
+
+    it 'returns a 200 status code' do
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the 'show' template" do
+      expect(response).to render_template('show')
+    end
+
+    it "includes 'Specific post by a Specific User' in the response body" do
+      expect(response.body).to include('test post')
+    end
+  end
+end
