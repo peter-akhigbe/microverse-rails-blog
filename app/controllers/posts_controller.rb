6c4560463cfe5d82d8e49e81bbc042ef6017afe9
Peter Akhigbe <akhigbe37@gmail.com>
2023-12-22 09:01:19 +0100

    Fix n+1 problem in PostsController#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,33 @@
 class PostsController < ApplicationController
+  before_action :set_user, only: %i[index show new create]
+
   def index
-    @user = User.find(params[:user_id])
-    @posts = User.find(params[:user_id]).posts
+    @posts = Post.includes(:comments, :author).where(author_id: params[:user_id])
   end
 
   def show
-    @post = Post.includes(:author).find_by(author_id: params[:user_id], id: params[:id])
-    @user = @post.author
-    @comments = Post.find(@post.id).comments
+    @post = Post.includes(:author, :comments).find_by(author_id: params[:user_id], id: params[:id])
+    @comments = @post.comments
   end
 
   def new
-    @user = current_user
     @post = Post.new
   end
 
   def create
-    @user = current_user
     @post = @user.posts.new(post_params)
     if @post.save
-      redirect_to user_post_path(@user, @post)
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
     else
-      puts @user
-      puts @post.errors.full_messages
-      flash.now[:errors] = 'Invalid post!'
       render :new
     end
   end
 
   private
+
+  def set_user
+    @user = current_user
+  end
 
   def post_params
     params.require(:post).permit(:title, :text)
